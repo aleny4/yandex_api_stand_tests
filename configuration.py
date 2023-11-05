292e7a15164d74c90b26cbebc2619a5f3cca01c1
aleny4 <149536286+aleny4@users.noreply.github.com>
2023-11-05 03:10:05 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 URL_SERVICE = "https://f1bfcc15-15bb-4e8e-b767-576b56f781f7.serverhub.praktikum-services.ru"
-DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
+CREATE_KITS_PATH = "/api/v1/kits"
