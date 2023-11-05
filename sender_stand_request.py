d283707d20eb677ed49cf3942a304497944ed00f
aleny4 <149536286+aleny4@users.noreply.github.com>
2023-11-05 17:00:59 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,18 +9,7 @@ def post_new_user():
                          headers=data.headers) # заголовки
 
 # создание нового набора
-def post_new_client_kit(kit_name, auth_token):
-    kit_body = data.kit_body.copy()
-    kit_body["name"] = kit_name
-    headers_kit = data.headers_kit.copy()
-    headers_kit["Authorization"] = "Bearer "+auth_token
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH, # подставляем полный url
-                         json=kit_body,
-                         headers=headers_kit)
-
-# создание набора с путсым телом
-def post_new_client_kit_empty_body(auth_token):
-    kit_body = data.empty_body.copy()
+def post_new_kit(kit_body, auth_token):
     headers_kit = data.headers_kit.copy()
     headers_kit["Authorization"] = "Bearer "+auth_token
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH, # подставляем полный url
