976c02c01d27761f35e79f5f07c268da7e6326e8
aleny4 <149536286+aleny4@users.noreply.github.com>
2023-11-05 17:03:19 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,22 @@
+# Заголовок
 headers = {
     "Content-Type": "application/json"
 }
 
+# Заголовок набора
+kit_headers = {
+    "Content-Type": "application/json",
+    "Authorization": ""
+}
+
+# Тело запроса на создание нового пользователя
 user_body = {
     "firstName": "Анатолий",
     "phone": "79995553322",
     "address": "г. Москва, ул. Пушкина, д. 10"
 }
 
+# Тело запроса на создание нового набора
 kit_body = {
-    "name": "test kit name"
+    "name": "test"
 }
