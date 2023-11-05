cb8b5356debe6781705f0bc2b6219603e7dd2519
aleny4 <149536286+aleny4@users.noreply.github.com>
2023-11-05 03:40:02 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,26 +2,27 @@ import configuration
 import requests
 import data
 
-def get_docs():
-    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+# создание нового пользователя
+def post_new_user():
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH, # подставляем полный url
+                         json=data.user_body,  # тело
+                         headers=data.headers) # заголовки
 
-def get_logs():
-        return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
-                            params={"count": 20})
+# создание нового набора
+def post_new_client_kit(kit_name, auth_token):
+    kit_body = data.kit_body.copy()
+    kit_body["name"] = kit_name
+    headers_kit = data.headers_kit.copy()
+    headers_kit["Authorization"] = "Bearer "+auth_token
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH, # подставляем полный url
+                         json=kit_body,
+                         headers=headers_kit)
 
-def get_users_table():
-        return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
-
-
-def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
-                         json=body,  # тут тело
-                         headers=data.headers)  # а здесь заголовки
-
-def post_products_kits(products_ids):
-        return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
-                             headers=data.headers)
-
-    response = post_products_kits(data.product_ids);
-    print(response.status_code)
-    print(response.json())
+# создание набора с путсым телом
+def post_new_client_kit_empty_body(auth_token):
+    kit_body = data.empty_body.copy()
+    headers_kit = data.headers_kit.copy()
+    headers_kit["Authorization"] = "Bearer "+auth_token
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH, # подставляем полный url
+                         json=kit_body,
+                         headers=headers_kit)
